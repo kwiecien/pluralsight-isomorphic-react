0d373a9a682e1e3e7a4fe7890fe04a50744ddba4
kwiecien <k.d.kwiecinski@gmail.com>
2019-02-10 21:04:46 +0100

    6: add Hot Reloading

--- src/index.jsx
+++ src/index.jsx
@@ -16,5 +16,12 @@ const render = (_App) => {
         document.getElementById('AppContainer'));
 };
 
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default;
+        render(NextApp);
+    });
+}
+
 render(App);
 fetchDataForLocation();
