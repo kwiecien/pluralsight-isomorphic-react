89e85eac416af90f66dc1fb43a368b584d62d239
kwiecien <k.d.kwiecinski@gmail.com>
2019-02-10 20:20:57 +0100

    6: add redux saga

--- src/getStore.js
+++ src/getStore.js
@@ -1,8 +1,17 @@
-import {createStore} from 'redux';
-import {identity} from 'lodash';
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import {createLogger} from 'redux-logger';
+import fetchQuestionsSaga from './sagas/fetch-questions.saga'
+import * as reducers from './reducers';
 
-export default function (defaultState = {
-    test: "Test Value"
-}) {
-    return createStore(identity, defaultState);
+export default function (defaultState) {
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewareChain = [sagaMiddleware];
+    if (process.env.NODE_ENV === 'development') {
+        const logger = createLogger();
+        middlewareChain.push(logger);
+    }
+    const store = createStore(combineReducers({...reducers}), defaultState, applyMiddleware(...middlewareChain));
+    sagaMiddleware.run(fetchQuestionsSaga);
+    return store;
 }
