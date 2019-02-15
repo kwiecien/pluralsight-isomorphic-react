3daa96f059b3c199471cfdfafd7a9d42c6d7b733
kwiecien <k.d.kwiecinski@gmail.com>
2019-02-15 18:02:48 +0100

    6: add React Router Part I: The Client

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import getStore from './getStore';
 import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
-const store = getStore();
+const history = createHistory();
+const store = getStore(history);
 const fetchDataForLocation = () => {
     store.dispatch({type: 'REQUEST_FETCH_QUESTIONS'});
 };
 const render = (_App) => {
     ReactDOM.render(
         <Provider store={store}>
-            <_App />
+            <ConnectedRouter history={history}>
+                <_App/>
+            </ConnectedRouter>
         </Provider>,
         document.getElementById('AppContainer'));
 };
